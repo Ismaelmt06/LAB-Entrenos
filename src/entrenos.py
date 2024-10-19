428b659d1eb3bbe585ac1d89ad5170532a2664d4
Ismaelmt06 <estifencarri117@gmail.com>
2024-10-19 17:44:54 +0200

    Entrenos 3

--- src/entrenos.py
+++ src/entrenos.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import csv
 from datetime import datetime
 Entreno = namedtuple('Entreno', 'tipo, fechahora, ubicacion, duracion, calorias, distancia, frecuencia, compartido')
+
 def lee_entrenos(ruta_csv):
     entrenos = []
     with open(ruta_csv, encoding= 'utf-8') as f:
